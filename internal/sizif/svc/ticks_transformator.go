f918bcd8bb867d33727ebe8360de5c8e8df197fb
ivansidor <i.sidorenko@vk.team>
2025-02-02 03:11:58 +0300

    f

--- internal/sizif/svc/ticks_transformator.go
+++ internal/sizif/svc/ticks_transformator.go
@@ -4,6 +4,7 @@ import (
 	"DeltaReceiver/internal/common/model"
 	bmodel "DeltaReceiver/pkg/binance/model"
 	"DeltaReceiver/pkg/log"
+	"encoding/json"
 	"fmt"
 	"sort"
 
@@ -36,22 +37,34 @@ func (s BookTicksTransformator) Transform(ticks []bmodel.SymbolTick, key *model.
 	}
 	validByTimeRange := true
 	if ticksOutsideTimeRange > 0 {
+		s.logger.Warn(fmt.Sprintf("invalid time range for %s key", key))
 		validByTimeRange = false
 	}
 	sort.Slice(ticks, func(i, j int) bool {
-		return ticks[i].UpdateId < ticks[j].UpdateId
+		return ticks[i].UpdateId < ticks[j].UpdateId || (ticks[i].UpdateId == ticks[j].UpdateId && ticks[i].Timestamp < ticks[j].UpdateId)
 	})
 	tickUpdateIds := make(map[int64]struct{})
+	var pqtTicks []bmodel.SymbolTick
+	pqtTicks = append(pqtTicks, ticks[0])
 	updateIdDuplicates := 0
-	for i := 0; i < len(ticks); i++ {
-		if _, ok := tickUpdateIds[ticks[i].UpdateId]; ok {
-			updateIdDuplicates++
+	for i := 1; i < len(ticks); i++ {
+		curTick := ticks[i]
+		if _, ok := tickUpdateIds[curTick.UpdateId]; ok {
+			prevTick := pqtTicks[len(pqtTicks)-1]
+			if prevTick.AskPrice != curTick.AskPrice || prevTick.AskQuantity != curTick.AskQuantity || prevTick.BidPrice != curTick.BidPrice || prevTick.BidQuantity != curTick.BidQuantity {
+				updateIdDuplicates++
+				pqtTicks = append(pqtTicks, curTick)
+			}
 		} else {
 			tickUpdateIds[ticks[i].UpdateId] = struct{}{}
+			pqtTicks = append(pqtTicks, curTick)
 		}
 	}
 	validByDuplicates := true
 	if updateIdDuplicates > 0 {
+		s.logger.Warn(fmt.Sprintf("invalid by duplicates for %s key", key))
+		data, _ := json.Marshal(pqtTicks)
+		s.logger.Info(fmt.Sprintf("ticks batch, %s", string(data)))
 		validByDuplicates = false
 	}
 	return ticks, validByDuplicates && validByTimeRange
