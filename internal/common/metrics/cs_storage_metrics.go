e645df049ae7b057faec6075270fb66eb68a1a41
ivansidor <i.sidorenko@vk.team>
2025-02-10 10:11:57 +0300

    fix race in metrics init

--- internal/common/metrics/cs_storage_metrics.go
+++ internal/common/metrics/cs_storage_metrics.go
@@ -5,52 +5,45 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-const SocratesMetricsNamespace string = "socrates"
+const NestorMetricsNamespace string = "nestor"
 
 type CsStorageMetrics struct {
-	metricsLabels                 map[string]string
 	errorCounter                  prometheus.Counter
 	dataBatchInsertLatencySummary prometheus.Summary
 	insertQueryLatencySummary     prometheus.Summary
 }
 
 func NewCsStorageMetrics(storageName string) *CsStorageMetrics {
+	metricsLabels := map[string]string{"table_name": storageName}
 	return &CsStorageMetrics{
-		metricsLabels: map[string]string{"table_name": storageName},
+		errorCounter: promauto.NewCounter(prometheus.CounterOpts{
+			Namespace:   NestorMetricsNamespace,
+			Name:        "insert_errors",
+			ConstLabels: metricsLabels,
+		}),
+		dataBatchInsertLatencySummary: promauto.NewSummary(prometheus.SummaryOpts{
+			Namespace:   NestorMetricsNamespace,
+			Name:        "insert_latency",
+			ConstLabels: metricsLabels,
+			Objectives:  map[float64]float64{0.5: 0.01, 0.75: 0.01, 0.90: 0.01, 0.95: 0.01, 0.99: 0.01},
+		}),
+		insertQueryLatencySummary: promauto.NewSummary(prometheus.SummaryOpts{
+			Namespace:   NestorMetricsNamespace,
+			Name:        "insert_latency",
+			ConstLabels: metricsLabels,
+			Objectives:  map[float64]float64{0.5: 0.01, 0.75: 0.01, 0.90: 0.01, 0.95: 0.01, 0.99: 0.01},
+		}),
 	}
 }
 
 func (s CsStorageMetrics) IncErrCount() {
-	if s.errorCounter == nil {
-		s.errorCounter = promauto.NewCounter(prometheus.CounterOpts{
-			Namespace:   SocratesMetricsNamespace,
-			Name:        "insert_errors",
-			ConstLabels: s.metricsLabels,
-		})
-	}
 	s.errorCounter.Inc()
 }
 
 func (s CsStorageMetrics) UpdInsertDataBatchLatency(latencyMs int64) {
-	if s.dataBatchInsertLatencySummary == nil {
-		s.dataBatchInsertLatencySummary = promauto.NewSummary(prometheus.SummaryOpts{
-			Namespace:   SocratesMetricsNamespace,
-			Name:        "insert_latency",
-			ConstLabels: s.metricsLabels,
-			Objectives:  map[float64]float64{0.5: 0.01, 0.75: 0.01, 0.90: 0.01, 0.95: 0.01, 0.99: 0.01},
-		})
-	}
 	s.dataBatchInsertLatencySummary.Observe(float64(latencyMs))
 }
 
 func (s CsStorageMetrics) UpdInsertQueryLatency(latencyMs int64) {
-	if s.insertQueryLatencySummary == nil {
-		s.insertQueryLatencySummary = promauto.NewSummary(prometheus.SummaryOpts{
-			Namespace:   SocratesMetricsNamespace,
-			Name:        "insert_latency",
-			ConstLabels: s.metricsLabels,
-			Objectives:  map[float64]float64{0.5: 0.01, 0.75: 0.01, 0.90: 0.01, 0.95: 0.01, 0.99: 0.01},
-		})
-	}
 	s.insertQueryLatencySummary.Observe(float64(latencyMs))
 }
